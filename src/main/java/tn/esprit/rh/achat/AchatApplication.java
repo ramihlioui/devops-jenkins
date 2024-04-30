5338a034ba61327a067ac29eb5065c694cf51eec
= <=>
2024-04-30 22:58:31 +0100

    added prometheus dependency

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,9 +3,11 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 
 @EnableScheduling
 @SpringBootApplication
+@EnablePrometheusEndpoint
 public class AchatApplication {
 
     public static void main(String[] args) {
