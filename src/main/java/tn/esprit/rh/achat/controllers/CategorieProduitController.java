694af315f4dbb7e13aed520e42f0df96792988f4
Ahmed CHERNI <ind@wevioo.com>
2024-05-10 09:11:15 +0100

    test demo

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @Api(tags = "Gestion des categories Produit")
-@RequestMapping("/categorieProduit")
+@RequestMapping("/categorieProduitss")
 public class CategorieProduitController {
 
 	@Autowired
